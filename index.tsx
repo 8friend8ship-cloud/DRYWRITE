58b0c366480257045c6be7b5ccd476fd98470ddc
8friend8ship-cloud <homedesigntaedi@gmail.com>
2026-08-27 14:14:29 +0900

    integrate(drywrite): wire language bridge into canonical app

--- index.tsx
+++ index.tsx
@@ -1,17 +1,18 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import FrontLanguageBotBridge from './FrontLanguageBotBridge';
 import './styles.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
+    <FrontLanguageBotBridge appId="APP_DRYWRITE" />
   </React.StrictMode>
 );
